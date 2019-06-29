f67d5eac25544a85751140250b4f6b3f1429d1fe
DemoliciousDt <league@Thes-Mac-mini.local>
2019-06-29 11:28:56 -0700

    Finished Houses

--- src/_01_houses/Houses.java
+++ src/_01_houses/Houses.java
@@ -1,5 +1,7 @@
 package _01_houses;
 
+import javax.swing.JOptionPane;
+
 import org.jointheleague.graphical.robot.Robot;
 
 public class Houses {
@@ -7,16 +9,29 @@ public class Houses {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		josh.moveTo(100, 800);
+		josh.moveTo(100, 500);
 		josh.setSpeed(100);	
+		josh.setWindowColor(0, 0, 0);
+		String height = JOptionPane.showInputDialog("What size house would you like between small, medium, and large?");
+		if (height.equalsIgnoreCase("Small")){
+			smallhouse();
+		}else if (height.equalsIgnoreCase("Medium")) {
+			mediumhouse();
+		}else if (height.equalsIgnoreCase("Large")) {
+			largehouse();
+		}else {
+			JOptionPane.showMessageDialog(null, "Sorry I can't do that.");
+		}
 	}
 	static void smallhouse() {
 		josh.penDown();
 		josh.setPenColor(0, 0, 255);
 		josh.move(60);
+		josh.turn(45);
+		josh.move(25);
 		josh.turn(90);
-		josh.move(50);
-		josh.turn(90);
+		josh.move(25);
+		josh.turn(45);
 		josh.move(60);
 		josh.turn(-90);
 		josh.setPenColor(0, 255, 0);
@@ -28,9 +43,11 @@ public class Houses {
 		josh.penDown();
 		josh.setPenColor(0, 0, 255);
 		josh.move(120);
+		josh.turn(45);
+		josh.move(25);
 		josh.turn(90);
-		josh.move(50);
-		josh.turn(90);
+		josh.move(25);
+		josh.turn(45);
 		josh.move(120);
 		josh.turn(-90);
 		josh.setPenColor(0, 255, 0);
