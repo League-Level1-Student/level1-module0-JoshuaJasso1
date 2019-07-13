791afba9d193d2dbd179bb52de49e587d5e1083a
League <league@Groovy.local>
2019-07-13 11:25:22 -0700

    Update JackInTheBox.java
    got the second step done for jack in the box

--- src/_01_houses/JackInTheBox.java
+++ src/_01_houses/JackInTheBox.java
@@ -1,36 +1,65 @@
 package _01_houses;
 
-public class JackInTheBox {
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.net.URL;
+
+import javax.swing.Icon;
+import javax.swing.ImageIcon;
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+
+public class JackInTheBox implements ActionListener {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		private void showPicture(String fileName) {
-		    try {
-		        JLabel imageLabel = createLabelImage(fileName);
-		        JFrame frame = new JFrame();
-		        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		        frame.add(imageLabel);
-		        frame.setVisible(true);
-		    } catch (Exception e) {
-		        e.printStackTrace();
-		    }
+		new JackInTheBox().showButton();
+	}
+
+	public void showButton() {
+		System.out.println("Button clicked");
+		JFrame frame = new JFrame();
+		frame.setVisible(true);
+		JButton button = new JButton("Surprise");
+		frame.add(button);
+		frame.pack();
+		button.addActionListener(this);
+	}
+
+	private void showPicture(String fileName) {
+		try {
+			JLabel imageLabel = createLabelImage(fileName);
+			JFrame frame = new JFrame();
+			frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+			frame.add(imageLabel);
+			frame.setVisible(true);
+		} catch (Exception e) {
+			e.printStackTrace();
 		}
-		private JLabel createLabelImage(String fileName) {
-			try {
-		            URL imageURL = getClass().getResource(fileName);
-		            if (imageURL == null) {
-			       System.err.println("Could not find image " + fileName);
-			       return new JLabel();
-		            } else {
-		                Icon icon = new ImageIcon(imageURL);
-		                JLabel imageLabel = new JLabel(icon);
-		                return imageLabel;
-		            }
-		        } catch (Exception e) {
-		            System.err.println("Could not find image " + fileName);
-		            return new JLabel();
-		        }
+
+	}
+
+	private JLabel createLabelImage(String fileName) {
+		try {
+			URL imageURL = getClass().getResource(fileName);
+			if (imageURL == null) {
+				System.err.println("Could not find image " + fileName);
+				return new JLabel();
+			} else {
+				Icon icon = new ImageIcon(imageURL);
+				JLabel imageLabel = new JLabel(icon);
+				return imageLabel;
+			}
+		} catch (Exception e) {
+			System.err.println("Could not find image " + fileName);
+			return new JLabel();
 		}
 	}
 
+	@Override
+	public void actionPerformed(ActionEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
 }
